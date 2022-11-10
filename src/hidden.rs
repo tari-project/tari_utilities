334e7054284a394a95e46df876412e26fa9cc388
Aaron Feickert <66188213+AaronFeickert@users.noreply.github.com>
2022-11-10 16:35:03 -0600

    Documentation and tests

--- src/hidden.rs
+++ src/hidden.rs
@@ -20,8 +20,11 @@
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 // USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-//! A generic type that hides data from being displayed or written, keeps it on the heap, zeroizes it when it goes away,
-//! limits access, and allows for type enforcement.
+//! Sometimes you need to handle sensitive data, like a passphrase or key material.
+//! There are pitfalls here that you want to avoid: the data should be zeroized when it goes out of scope, shouldn't be
+//! displayed or output to debug logs, shouldn't be unintentionally copied or moved, and often should have strict type
+//! differentiation to avoid it being misused in an unintended context. This library provides a generic type that can
+//! help.
 
 use std::{any::type_name, fmt, marker::PhantomData};
 
@@ -31,7 +34,24 @@ use zeroize::Zeroize;
 /// A marker trait for labeling different hidden types
 pub trait HiddenLabel {}
 
-/// Create a hidden type label
+/// This is a macro that produces a label for a hidden type.
+/// You can use it to generate multiple hidden types that can't be unintentionally mixed up in things like function
+/// definitions. You shouldn't ever need to access it directly once you define your hidden type, since it only exists to
+/// keep types in order.
+///
+/// Its use is optional; as shown elsewhere in this documentation, you can define a hidden type without the use of a
+/// label, in which a default one will be applied for you. But if you do that, you don't get the type enforcement
+/// benefits.
+///
+/// ```edition2018
+/// # #[macro_use] extern crate tari_utilities;
+/// # use tari_utilities::hidden::HiddenLabel;
+/// # use tari_utilities::hidden_label;
+/// # fn main() {
+/// // This makes `MyHiddenTypeLabel` available for later use when defining a hidden type
+/// hidden_label!(MyHiddenTypeLabel);
+/// # }
+/// ```
 #[macro_export]
 macro_rules! hidden_label {
     ($name:ident) => {
@@ -42,10 +62,95 @@ macro_rules! hidden_label {
     };
 }
 
-// A default hidden type label; only use this if you're absolutely sure you don't care about type enforcement
+// This is the hidden type label that is used internally if you define a hidden type without proving your own label
 hidden_label!(DefaultHiddenLabel);
 
-/// Data that needs to be kept hidden, needs to be zeroized when it goes away, and is resistant to misuse
+/// A generic type for data that needs to be kept hidden, needs to be zeroized when out of scope, and is resistant to
+/// misuse.
+///
+/// You can define a hidden type using any underlying type that supports `Default` and `Zeroize`.
+/// This is the case for most basic types that you probably care about.
+///
+/// You can optionally supply a label to the hidden type, which lets you define multiple hidden types that can't be
+/// interchanged. To do this, use the `hidden_label!` macro to define a label, and then use it when defining your hidden
+/// type. If you don't include a label, a default one is used for you.
+///
+/// Hidden data can't be copied, which is an intentional design decision, and you can only access it as a reference.
+/// However, it supports other functionality transparently: cloning, equality, ordering, serialization, and the like.
+/// Note an important fact about serialization: it's transparent, so the type label isn't serialized.
+/// This means you should be sure to know the label you intend during deserialization!
+///
+/// ```edition2018
+/// # use rand::rngs::OsRng;
+/// # use rand::RngCore;
+/// # use tari_utilities::hidden::{Hidden, HiddenLabel};
+/// # use tari_utilities::hidden_label;
+///
+/// // In this example, we need to handle keys for a cipher that are `[u8; 32]` byte arrays
+///
+/// // Define a label for the new hidden type
+/// // This ensures that any other `[u8; 32]` hidden types can't get confused with this one
+/// hidden_label!(CipherKeyLabel);
+///
+/// // Alias a new hidden type using the data type and label
+/// type CipherKey = Hidden<[u8; 32], CipherKeyLabel>;
+///
+/// // We can create hidden data from existing data; in this case, it's the caller's responsibility to make sure the existing data is handled securely
+/// let key_from_data = CipherKey::hide([1u8; 32]);
+///
+/// // We can access the hidden data as a reference, but not take ownership of it
+/// assert_eq!(key_from_data.reveal(), &[1u8; 32]);
+///
+/// // We can create default hidden data and then modify it as a mutable reference; this is common for functions that act on data in place
+/// let mut rng = OsRng;
+/// let mut key_in_place = CipherKey::default();
+/// rng.fill_bytes(key_in_place.reveal_mut());
+///
+/// // Cloning is safe to do
+/// let clone = key_in_place.clone();
+/// assert_eq!(key_in_place.reveal(), clone.reveal());
+/// ```
+///
+/// Type enforcement means the compiler won't let you mix and match hidden types with different labels.
+/// This can be important when dealing with different types of key material, for example.
+///
+/// ```compile_fail
+/// # use tari_utilities::hidden::{Hidden, HiddenLabel};
+/// # use tari_utilities::hidden_label;
+///
+/// // These are labels for different hidden types we want to create; in this case, keys for two different ciphers
+/// hidden_label!(KeyForCipherALabel);
+/// hidden_label!(KeyForCipherBLabel);
+///
+/// // Both ciphers have keys that are `[u8; 32]` under the hood
+/// type KeyForCipherA = Hidden<[u8; 32], KeyForCipherALabel);
+/// type KeyForCipherB = Hidden<[u8; 32], KeyForCipherBLabel);
+///
+/// // Create a key for each cipher; notice the underlying data is the same for both
+/// let key_a = KeyForCipherA::hide([1u8; 32]);
+/// let key_b = KeyForCipherA::hide([1u8; 32]);
+///
+/// // The compiler won't let us treat them the same; this won't build
+/// assert_eq!(key_a.reveal(), key_b.reveal());
+/// ```
+///
+/// But if you choose not to use a label, you can mix and match hidden types. Be careful if you do this!
+///
+/// ```edition2018
+/// # use tari_utilities::hidden::{Hidden, HiddenLabel};
+/// # use tari_utilities::hidden_label;
+///
+/// // Define two types with no labels; you probably don't want to actually do this!
+/// type TypeA = Hidden<[u8; 32]>;
+/// type TypeB = Hidden<[u8; 32]>;
+///
+/// // Create an instance of both
+/// let a = TypeA::default();
+/// let b = TypeB::default();
+///
+/// // You can mix and match these! Be sure that's what you actually intended.
+/// assert_eq!(a.reveal(), b.reveal());
+/// ```
 #[derive(Clone, Default, Deserialize, Eq, Ord, PartialEq, PartialOrd, Serialize)]
 #[serde(transparent)]
 pub struct Hidden<T, L = DefaultHiddenLabel>
@@ -63,7 +168,7 @@ where
     T: Zeroize,
     L: HiddenLabel,
 {
-    /// Hide the data
+    /// Create new hidden data from the underlying type
     pub fn hide(inner: T) -> Self {
         Self {
             inner: Box::new(inner),
@@ -82,7 +187,7 @@ where
     }
 }
 
-/// Only output masked data for debugging
+/// Only output masked data for debugging, keeping the hidden data hidden
 impl<T, L> fmt::Debug for Hidden<T, L>
 where
     T: Zeroize,
@@ -93,7 +198,7 @@ where
     }
 }
 
-/// Only display masked data
+/// Only display masked data, keeping the hidden data hidden
 impl<T, L> fmt::Display for Hidden<T, L>
 where
     T: Zeroize,
@@ -123,5 +228,44 @@ where
 {
     fn drop(&mut self) {
         self.zeroize();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn equality() {
+        assert_eq!(Hidden::<u8>::hide(0u8), Hidden::<u8>::hide(0u8));
+        assert_ne!(Hidden::<u8>::hide(0u8), Hidden::<u8>::hide(1u8));
+    }
+
+    #[test]
+    fn reference() {
+        assert_eq!(Hidden::<u8>::hide(0u8).reveal(), &0u8);
+        assert_ne!(Hidden::<u8>::hide(0u8).reveal(), &1u8);
+    }
+
+    #[test]
+    fn serialize() {
+        let hidden = Hidden::<u8>::hide(1u8);
+        let ser = serde_json::to_string(&hidden).unwrap();
+
+        let deser: Hidden<u8> = serde_json::from_str(&ser).unwrap();
+        assert_eq!(hidden, deser);
+
+        // You can deserialize with any label
+        hidden_label!(TestLabel);
+        let deser_label: Hidden<u8, TestLabel> = serde_json::from_str(&ser).unwrap();
+        assert_eq!(hidden.reveal(), deser_label.reveal());
+    }
+
+    #[test]
+    fn masking() {
+        let hidden = Hidden::<u8>::hide(1u8);
+        let formatted = format!("{}", hidden);
+        let expected = format!("Hidden<{}, {}>", type_name::<u8>(), type_name::<DefaultHiddenLabel>());
+        assert_eq!(formatted, expected);
     }
 }
--- src/password.rs
+++ src/password.rs
@@ -1,4 +1,4 @@
-//! A module with a safe password wrapper.
+//! A type for handling a passphrase safely.
 
 use std::{error::Error, fmt::Display, str::FromStr};
 
@@ -9,7 +9,24 @@ use crate::{
 
 hidden_label!(SafePasswordLabel);
 
-/// A hidden password type that zeroizes when it goes away
+/// A representation of a passphrase that zeroizes on drop, prevents display and debug output, and limits access to
+/// references
+///
+/// The passphrase can be instantiated from a string or any type that can become a string.
+/// It is converted to a byte array, which can be accessed as a mutable or immutable reference.
+///
+/// ```edition2018
+/// # use tari_utilities::SafePassword
+///
+/// // Create a safe passphrase
+/// let passphrase = SafePassword::from("my secret passphrase");
+///
+/// // We can also use a string directly
+/// assert_eq!(
+///     passphrase.reveal(),
+///     SafePassword::from("my secret passphrase".to_string())
+/// );
+/// ```
 pub type SafePassword = Hidden<Vec<u8>, SafePasswordLabel>;
 
 /// An error for parsing a password from a string
@@ -35,5 +52,24 @@ impl FromStr for SafePassword {
 impl<S: Into<String>> From<S> for SafePassword {
     fn from(s: S) -> Self {
         Self::hide(s.into().into_bytes())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use super::SafePassword;
+
+    #[test]
+    fn from_strings() {
+        let password = "password";
+
+        let from_str = SafePassword::from_str(password).unwrap();
+        let from_string = SafePassword::from(password.to_string());
+        let from_string_ref = SafePassword::from(password);
+
+        assert_eq!(from_str, from_string);
+        assert_eq!(from_string, from_string_ref);
     }
 }
